09788602b3031662b709f4562a07e59155d7a66e
Aravinth-thunder <varavinth321@gmail.com>
2020-05-14 09:44:21 +0530

    Updates

---
+++ precache-manifest.fd3a369b7ba3401cdeccee97f10d79d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ebdfbf539922af484f98c6c77a3923",
+    "url": "/ToDo-react/index.html"
+  },
+  {
+    "revision": "3b2be23e90a3e8fee8f9",
+    "url": "/ToDo-react/static/css/main.0b2a76e8.chunk.css"
+  },
+  {
+    "revision": "46982b33b41813fb628a",
+    "url": "/ToDo-react/static/js/2.6f8b097f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-react/static/js/2.6f8b097f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b2be23e90a3e8fee8f9",
+    "url": "/ToDo-react/static/js/main.830d1e2a.chunk.js"
+  },
+  {
+    "revision": "f0a5ac2331149d532f3b",
+    "url": "/ToDo-react/static/js/runtime-main.42258b56.js"
+  }
+]);
